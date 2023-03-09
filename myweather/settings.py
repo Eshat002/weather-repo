73924672037f6af2998f0586fcbdfbe74ec72278
eshat <cristophernehal@yahoo.com>
2023-03-09 19:55:53 +0600

    changed allowed host

--- myweather/settings.py
+++ myweather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['earth13.herokuapp.com','127.0.0.1','test-kmx5.onrender.com']
+ALLOWED_HOSTS = ['earth13.herokuapp.com','127.0.0.1','weather-app-1.onrender.com']
 
 
 # Application definition
